50e08ec4c96eb777b27651ffd4aa1004eb732884
Vladymyr-Cherervko <vladymyrcherevko@gmail.com>
2020-11-12 14:09:00 +0200

    added new generic repository interface

---
+++ ProductCompatibility/Data/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductCompatibility.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCompatibility.Data.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> All { get; }
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);        
+        Task<TEntity> FindByIdAsync(int id);
+    }
+}
